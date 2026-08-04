d51337e60740b677d1b5484ad2ac90e0ebc71ed9
peppa3109-byte <peppa3109@gmail.com>
2026-08-04 13:00:58 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "🥴 Envenenado RP | Bot",     // this is what people actually see
+        state: "🥴 XFile Leaks | Bot",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
